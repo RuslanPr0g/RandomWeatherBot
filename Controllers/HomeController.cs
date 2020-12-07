667890c653244a3ceacec655acaff077e9843f9a
RuslanPr0g <theruslan.prog@gmail.com>
2020-12-07 17:37:37 +0300

    Add command

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,23 +8,9 @@ namespace RandWeatherBot.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public string Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return "попадется твой город - повезло";
         }
     }
 }
