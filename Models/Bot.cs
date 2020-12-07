a4cb044889769e4e30183f1c34b451f68f955e3f
RuslanPr0g <theruslan.prog@gmail.com>
2020-12-07 17:23:04 +0300

    Bot class fixed

--- Models/Bot.cs
+++ Models/Bot.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using RandWeatherBot.Models.Commands;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Telegram.Bot;
 
 namespace RandWeatherBot.Models
@@ -6,6 +8,9 @@ namespace RandWeatherBot.Models
     public static class Bot
     {
         private static TelegramBotClient client;
+        private static List<Command> commandsList;
+
+        public static IReadOnlyList<Command> Commands { get => commandsList.AsReadOnly(); }
 
         public static async Task<TelegramBotClient> Get()
         {
@@ -14,7 +19,10 @@ namespace RandWeatherBot.Models
                 return client;
             }
 
-            // todo: 
+            commandsList = new List<Command>();
+            commandsList.Add(new HelloCommand());
+
+            // TODO: ADD MORE COMMANDS
 
             client = new TelegramBotClient(AppSettings.Key);
             await client.SetWebhookAsync("");
